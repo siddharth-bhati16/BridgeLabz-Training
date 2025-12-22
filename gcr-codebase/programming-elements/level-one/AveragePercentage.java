618c9726eb455ccbe23379995292788398bf1ae0
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-22 20:12:06 +0530

    [siddharth] : PerimeterOfRectangle

---
+++ gcr-codebase/programming-elements/level-one/AveragePercentage.java
@@ -0,0 +1,24 @@
+//calculating average percentage marks in pcm
+import java.util.Scanner;
+
+public class AveragePercentage
+{
+	public static void main (String []args){
+	
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the physicmarks");
+		int System.out.println("Enter the physicmarks");
+		int physicmarks= sc.nextInt();= sc.nextInt();
+		
+		System.out.println("Enter the chemistrymarks");
+		int chemistrymarksmarks= sc.nextInt();
+		
+		System.out.println("Enter the mathsmarks");
+		int mathsmarksmarks= sc.nextInt();
+		
+		int AveragePercentage= (physicmarks + chemistrymarks + mathsmarks)/3;
+ 	
+		System.out.println("currentage marks is"AveragePercentage);
+	}
+}
