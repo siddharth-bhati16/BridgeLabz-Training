ff92cae5bb4cdcc296ac2530503d1796b5fb3083
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-22 20:28:34 +0530

    [siddharth] :         DiscountedFees.java

---
+++ gcr-codebase/programming-elements/level-one/DiscountedFees.java
@@ -0,0 +1,18 @@
+//calculating discount fees
+import java.util.Scanner;
+
+public class DiscountedFees
+{
+	public static void main (String []args){
+	
+		
+		int Fees=125000;
+		int Discountpercentage=10;
+		
+		
+		int DiscountedFees=125000-((125000*10)/100);
+		System.out.println("discounted price is  " + DiscountedFees);
+		
+		
+	}
+}
