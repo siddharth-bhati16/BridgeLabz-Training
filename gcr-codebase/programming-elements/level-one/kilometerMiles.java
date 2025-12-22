17b47ab8078880af7efe16501700aa3eea4095a5
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-22 20:53:29 +0530

    [siddharth] : added 10.8km to miles

---
+++ gcr-codebase/programming-elements/level-one/kilometerMiles.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ public class kilometerMiles{
+ 
+  public static void main (String []args){
+  
+double kilometer= 10.8;
+double kilometerMiles= kilometer*0.631371;
+System.out.println("value of 10.8 kilometer in miles is  "+ kilometerMiles);
+}
+}
