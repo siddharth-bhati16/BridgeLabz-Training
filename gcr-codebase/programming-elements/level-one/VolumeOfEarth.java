1dd0541f84d9518f0ae05d9439ff2645e1fef97f
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-22 20:43:31 +0530

    [siddharth] :         VolumeOfEarth.java

---
+++ gcr-codebase/programming-elements/level-one/VolumeOfEarth.java
@@ -0,0 +1,17 @@
+//calculating volume of earth
+import java.util.Scanner;
+
+public class VolumeOfEarth
+{
+	public static void main (String []args){
+	
+	
+		int radius=6378;
+		Double volumeofearth=(4/3)*3.14*radius*radius*radius;
+		
+		
+		System.out.println("volume of earth is  " + volumeofearth);
+		
+		
+	}
+}
