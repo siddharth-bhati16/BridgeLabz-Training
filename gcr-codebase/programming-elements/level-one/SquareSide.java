0588e99575fbd136072d84c3f967d8bd678b1110
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-23 07:41:49 +0530

    [siddharth] : added side of a square

---
+++ gcr-codebase/programming-elements/level-one/SquareSide.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ public class SquareSide{
+ 
+  public static void main (String []args){
+  Scanner sc=new Scanner(System.in);
+   
+   System.out.println("perimeter of square is  ");
+   double perimeter=sc.nextDouble();
+
+   double side=perimeter/4;
+   System.out.println("side of the square is  "+ side);
+}
+}
