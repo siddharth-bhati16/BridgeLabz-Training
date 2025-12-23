a1dd5a4c387ba7e116827160aefe0ccb6f1a6a13
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-23 20:24:42 +0530

    [siddharth] : added reverse digit

---
+++ gcr-codebase/arrays/level-2/ReverseDigit.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ReverseDigit 
+{
+    public static void main(String args[])
+    {
+        //creating scanner object
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println("Enter a number: ");
+        int number= sc.nextInt();
+        int digit=0, temp=number;
+        while(temp>0)
+        {
+            digit++;
+            temp=temp/10;
+        }
+        int array[]= new int[digit];
+        for(int i=0; i<digit; i++)
+        {
+            array[i]= number % 10;
+            number= number / 10;
+        }
+        //displaying array in reverse order
+        for(int j=0; j<digit; j++)
+        {
+            System.out.print(array[j]);
+        }
+    }
+}
