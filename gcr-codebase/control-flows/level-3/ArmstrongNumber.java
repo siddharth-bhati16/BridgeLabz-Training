720a10bdea375a7a582344825205b31f2b4fc20f
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-23 16:59:59 +0530

    [siddharth] : added amstrong number

---
+++ gcr-codebase/control-flows/level-3/ArmstrongNumber.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class ArmstrongNumber
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the number");
+		int number = sc.nextInt();
+		int copy1 = number;
+		int copy2 = number;
+		int digit=0;
+		int calculatedsum=0;
+		
+		
+		
+		while(copy1 > 0)
+		{
+			copy1=copy1/10;
+			digit ++;
+		}
+		
+		while(number >0)
+		{
+			int answer = 1;
+			for(int i=digit; i>0; i--)
+			{
+				answer= answer * (number % 10);
+			}
+			calculatedsum = calculatedsum + answer;
+			number = number / 10;
+		}
+		
+		
+		if( calculatedsum == copy2)
+		{
+			System.out.println("Its an Armstrong number.");
+		}
+		else 
+		{
+			System.out.println("Its not an Armstrong number.");
+		}
+		
+		
+
+	}
+}
