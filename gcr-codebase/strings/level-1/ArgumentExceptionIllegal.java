8cbcb4613db441ff9b4e98d814401e571d097436
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-26 06:14:45 +0530

    [siddharth] : added illegal exception handling

---
+++ gcr-codebase/strings/level-1/ArgumentExceptionIllegal.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class ArgumentExceptionIllegal
+{
+
+	public static void illegalArg(String str)
+	{
+		 subString(str.length()-2,0);
+	}
+	public static void subString(int start, int end)
+	{
+		if(end<start)
+		{
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		try{
+			illegalArg(str);
+		}
+		catch(IllegalArgumentException exc)
+		{
+			System.out.println(exc);
+		}
+	}
+	
+}
