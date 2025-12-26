c02bc2963f8bdeb992415e6ef41c3d270a8f8ab1
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-26 16:05:30 +0530

    [siddharth] : added longest word in string

---
+++ gcr-codebase/Extras/part-1/LongestWord.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class LongestWord
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String str = sc.nextLine();
+        
+        String longestWord = getLongestWord(str);
+        System.out.println("The longest word is: " );
+		System.out.println(longestWord);
+        
+        sc.close();
+    }
+    
+    public static String getLongestWord(String str) 
+	{
+        String[] words = str.split(" ");
+        String longestWord = "";
+        
+        for (String word : words) 
+		{
+            if (word.length() > longestWord.length()) 
+			{
+                longestWord = word;
+            }
+        }
+        
+        return longestWord;
+    }
+}
