e2368698d18f0857d345125b9b81aaa0efbdc6eb
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-26 06:09:09 +0530

    [siddharth] : added exception in java

---
+++ gcr-codebase/strings/level-1/Exceptionn.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Exceptionn
+{
+	public static void main(String[] args)
+	{
+	 try{
+			stringMethod();
+			
+		}
+		catch(NullPointerException exc)
+		{
+			System.out.println(exc.toString());
+		}
+		
+	
+	}
+	
+	
+	public static void stringMethod() throws NullPointerException
+	{
+		String str = null;
+		char[] array = str.toCharArray();
+		
+	}
+
+	
+}
