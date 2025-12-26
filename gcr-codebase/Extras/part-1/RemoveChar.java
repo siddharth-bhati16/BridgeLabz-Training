2f6b2c5d9486451185933202997eaea3eba4437e
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-26 16:15:58 +0530

    [siddharth] : added remove character

---
+++ gcr-codebase/Extras/part-1/RemoveChar.java
@@ -0,0 +1,11 @@
+import.java.util.*;
+public class RemoveChar {
+    public static void main(String[] args) {
+        String str = "java programming";
+        char charToRemove = 'a';
+        
+        String result = str.replace(String.valueOf(charToRemove), "");
+        
+        System.out.println("Result: " + result); 
+		}
+		}	
