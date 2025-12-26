bb4de1063b30ac248e0baadd3fc2126b8fd0f0f0
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-26 06:18:30 +0530

    [siddharth] : added number format exception

---
+++ gcr-codebase/strings/level-1/NumberFormatExceptionn.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class NumberFormatExceptionn
+{
+
+	public static void generator(String str)
+	{
+		int a = Integer.parseInt(str);
+	}
+	
+	
+	public static void main(String[] args)
+	{
+	
+		//creating scanner object
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		//using try catch block for exception
+		
+		try{
+			generator(s);
+		}
+		catch(Exception exc)
+		{
+			System.out.println(exc);
+		}
+	}
+	
+	
+}
