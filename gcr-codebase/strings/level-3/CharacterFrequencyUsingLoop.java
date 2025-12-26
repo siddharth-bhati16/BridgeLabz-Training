8d377b58add2505c77a79f4e991cff0dfbf609e8
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-26 15:16:50 +0530

    [siddharth] : added finding character frequency using loop

---
+++ gcr-codebase/strings/level-3/CharacterFrequencyUsingLoop.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class CharacterFrequencyUsingLoop
+{
+    public static void main(String[] args)
+	{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+       
+
+        // Calculating length
+		
+		 int length = 0;
+        for (char c : str.toCharArray()) 
+		{
+            length++;
+        }
+
+        boolean[] visitedOrNot = new boolean[length];
+
+        System.out.println("Character frequencies:");
+        for (int i = 0; i < length; i++) 
+		{
+            if (visitedOrNot[i]) 
+			{
+                continue;
+            }
+            char currentChar = str.charAt(i);
+            int count = 1;
+
+            for (int j = i + 1; j < length; j++) 
+			{
+                if (str.charAt(j) == currentChar) 
+				{
+                    count++;
+                    visitedOrNot[j] = true;
+                }
+            }
+            System.out.println(currentChar + ": " + count);
+        }
+        sc.close();
+    }
+}
