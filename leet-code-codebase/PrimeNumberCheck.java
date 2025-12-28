317938d5cbc9e27d2542ae30bb106eea6cd6f393
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-28 13:29:56 +0530

    [siddharth] : added checking prime number

---
+++ leet-code-codebase/PrimeNumberCheck.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class PrimeNumberCheck
+{
+
+	public static boolean checkPrime(int n)
+	{
+		if(n<2)
+		{
+			return false;
+		}
+		for(int i =2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println("The number is Prime  "+ checkPrime(n));
+	}
+	
+	
+	
+}
