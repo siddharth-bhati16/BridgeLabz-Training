cb977021d91413e60c39620d9d0ba2773df0a5f7
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-28 13:50:17 +0530

    [siddharth] : added temprature conversion

---
+++ leet-code-codebase/TemperatureConversion.java
@@ -0,0 +1,21 @@
+mport java.util.*;
+public class TemperatureConversion
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		double temp = sc.nextDouble();
+		
+		System.out.println("Celecius to fahrenheit "+fahrenheitToCelsius(temp));
+		System.out.println("Fahrenheit to celsius "+celsiusToFahrenheit(temp));
+	}
+	 
+	public static double fahrenheitToCelsius(double f)
+	{
+        return (f - 32) * 5 / 9;
+    }
+
+    public static double celsiusToFahrenheit(double c)
+	{
+        return (c * 9 / 5) + 32;
+    }
