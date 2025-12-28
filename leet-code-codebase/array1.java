90e7419e9639805a55bde5a7297c7d86a703819a
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-28 13:22:40 +0530

    [siddharth] : added printing marks

---
+++ leet-code-codebase/array1.java
@@ -0,0 +1,13 @@
+//TO PRINT MARKS OF VARIOUS SUBJECT
+import java.util.*;
+public class array1{
+    public static void main(String[] args) {
+        int marks[]=new int [3];
+        marks[0]=56;
+        marks[1]=93;
+        marks[2]=88;
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
