22299f0c51775445b681cb8b960e7fe1653dbc7a
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-29 12:27:27 +0530

    [Siddharth] : testing the files

--- java-oops-practice/gcr-codebase/javaOopsConcept/src/com/oops/level/Circle.java
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/oops/level/Circle.java
@@ -1,9 +1,10 @@
 package com.oops.level;
 
-public class Circle {
+public class  Circle{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println ("sfhghjg,j");
 
 	}
 
