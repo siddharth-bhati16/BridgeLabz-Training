f7c25982508699f84f4a2b518d2114f75599bd13
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-31 12:02:12 +0530

    [siddharth] : added bank account

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/oops/level/BankAccount.java
@@ -0,0 +1,49 @@
+package com.oops.level;
+
+
+class BankAccount 
+{
+    public String accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolder, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+    }
+
+   
+    
+    public double getBalance() 
+    {
+        return balance;
+    }
+
+    
+    public void deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+        } 
+        else
+        {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    
+    public void withdraw(double amount)
+    {
+        if (amount > 0 && amount <= balance) 
+        {
+            balance -= amount;
+        } 
+        else 
+        {
+            System.out.println("Insufficient funds or invalid withdrawal amount.");
+        }
+    }
+}
+
