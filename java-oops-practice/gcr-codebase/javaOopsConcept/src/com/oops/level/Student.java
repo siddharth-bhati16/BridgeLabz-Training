9bd5474a584c5d1b23401baa54d89c8d5c18ff9a
siddharth-bhati16 <bsiddharth166@gmail.com>
2025-12-31 12:22:06 +0530

    [siddharth] : added student

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/oops/level/Student.java
@@ -0,0 +1,30 @@
+package com.oops.level;
+
+
+
+class Student 
+{
+    public int rollNumber;
+    protected String name;
+    private double CGPA;
+
+    // Constructor
+    public Student(int rollNumber, String name, double CGPA) 
+    {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+
+    // Public method to get CGPA
+    public double getCGPA() 
+    {
+        return CGPA;
+    }
+
+    // Public method to set CGPA
+    public void setCGPA(double CGPA) 
+    {
+        this.CGPA = CGPA;
+    }
+}
