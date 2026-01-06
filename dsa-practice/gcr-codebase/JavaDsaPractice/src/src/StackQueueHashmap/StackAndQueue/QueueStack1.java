fab2d0d080f70854b176c0fcb3ec85b330d179f3
siddharth-bhati16 <bsiddharth166@gmail.com>
2026-01-06 11:16:09 +0530

    [siddharth] : added queue using stack

--- dsa-practice/gcr-codebase/JavaDsaPractice/src/src/StackQueueHashmap/StackAndQueue/QueueStack1.java
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/src/StackQueueHashmap/StackAndQueue/QueueStack1.java
@@ -1,19 +1,60 @@
-
 package src.StackQueueHashmap.StackAndQueue;
+
 import java.util.Stack;
 
 public class QueueStack1 {
+
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    // Enqueue operation
+    public void enqueue(int x) {
+        s1.push(x);
+    }
+
+    // Dequeue operation
+    public int dequeue() {
+        if (s1.isEmpty() && s2.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+
+        return s2.pop();
+    }
+
+    // Peek operation
+    public int peek() {
+        if (s1.isEmpty() && s2.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+
+        return s2.peek();
+    }
+
     public static void main(String[] args) {
-        QueueUsingStacks queue = new QueueUsingStacks();
+        QueueStack1 queue = new QueueStack1();
 
         queue.enqueue(10);
         queue.enqueue(20);
         queue.enqueue(30);
 
-        System.out.println(queue.dequeue()); 
-        System.out.println(queue.peek());   
-        System.out.println(queue.dequeue()); 
-        System.out.println(queue.dequeue()); 
+        System.out.println(queue.dequeue()); // 10
+        System.out.println(queue.peek());    // 20
+        System.out.println(queue.dequeue()); // 20
+        System.out.println(queue.dequeue()); // 30
     }
 }
 
