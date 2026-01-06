0c14a1972dcd0cf0c3dd1c29678f3ab47194471b
siddharth-bhati16 <bsiddharth166@gmail.com>
2026-01-06 14:39:53 +0530

    [siddharth] : added span problem

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/src/StackQueueHashmap/StackAndQueue/SpanProblem.java
@@ -0,0 +1,36 @@
+package src.StackQueueHashmap.StackAndQueue;
+
+import java.util.Stack;
+import java.util.Arrays;
+
+public class SpanProblem {
+
+    public static int[] calculateSpan(int[] prices) {
+        int n = prices.length;
+        int[] span = new int[n]; 
+        Stack<Integer> stack = new Stack<>(); 
+
+        for (int i = 0; i < n; i++) {
+                        while (!stack.isEmpty() && prices[i] >= prices[stack.peek()]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                span[i] = i + 1; 
+            } else {
+                span[i] = i - stack.peek(); 
+            }
+
+            stack.push(i);
+        }
+
+        return span;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};
+        int[] span = calculateSpan(prices);
+        System.out.println("Stock Prices: " + Arrays.toString(prices));
+        System.out.println("Span:         " + Arrays.toString(span));
+    }
+}
