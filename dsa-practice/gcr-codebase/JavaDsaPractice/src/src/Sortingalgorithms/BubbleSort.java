117a72a8ce28ee6e62ed2a050ae6ecf9ac689899
siddharth-bhati16 <bsiddharth166@gmail.com>
2026-01-07 12:14:12 +0530

    [siddharth] : added bubble sort

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/src/Sortingalgorithms/BubbleSort.java
@@ -0,0 +1,25 @@
+package src.Sortingalgorithms;
+
+class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] marks = {78, 45, 89, 60, 55};
+
+        // Bubble Sort
+        for (int i = 0; i < marks.length - 1; i++) {
+            for (int j = 0; j < marks.length - 1 - i; j++) {
+                if (marks[j] > marks[j + 1]) {
+                    int temp = marks[j];
+                    marks[j] = marks[j + 1];
+                    marks[j + 1] = temp;
+                }
+            }
+        }
+
+        // Print sorted marks
+        System.out.println("Sorted Student Marks:");
+        for (int m : marks) {
+            System.out.print(m + " ");
+        }
+    }
+}
