8e30d690fc8b869632ce8710fd66c8cd334f1727
siddharth-bhati16 <bsiddharth166@gmail.com>
2026-01-09 11:29:15 +0530

    [siddharth] : added reverse string

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/src/linearBinarySearch/ReverseString.java
@@ -0,0 +1,20 @@
+package src.linearBinarySearch;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+        String input = "hello";
+        String reversed = reverseString(input);
+        System.out.println(reversed);
+    }
+
+    public static String reverseString(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(str);
+
+        sb.reverse();
+
+        return sb.toString();
+    }
+}
