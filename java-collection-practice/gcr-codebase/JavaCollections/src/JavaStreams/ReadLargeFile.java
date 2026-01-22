84f24f60f3c68d7bb50e62062c7fd62263b7b50a
siddharth-bhati16 <bsiddharth166@gmail.com>
2026-01-22 09:07:22 +0530

    [siddharth] : added reading large file

---
+++ java-collection-practice/gcr-codebase/JavaCollections/src/JavaStreams/ReadLargeFile.java
@@ -0,0 +1,36 @@
+package JavaStreams;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadLargeFile {
+
+    public static void main(String[] args) {
+
+        String filePath = "largefile.txt"; 
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                if (line.toLowerCase().contains("error")) {
+                    System.out.println(line);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading the file.");
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } catch (IOException e) {
+                System.out.println("Error closing the file.");
+            }
+        }
+    }
+}
+
